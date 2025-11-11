58e654079854d88ea3210e0b24972e3f31e324db
TimeWT <timetana624@gmail.com>
2025-11-12 00:16:08 +0700

    a

--- next.config.ts
+++ next.config.ts
@@ -21,9 +21,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  outputFileTracingIncludes: {
-    "/api/*": ["./node_modules/.prisma/client/*.wasm"],
-  },
+  
 };
 
 export default nextConfig;
